9d78a1ee45df3d84c29c05ddba8018edb1ede9da
Zer0Necrosis <nathan.niemala@live.com>
2019-07-18 02:58:38 -0500

    Update Start-ServerSync.bat
    Basic ServerSync Bat

--- Start-ServerSync.bat
+++ Start-ServerSync.bat
@@ -1,5 +1,5 @@
 @echo off
 SETLOCAL
 FOR %%f IN (serversync*.jar) DO (SET serversync=%%~nxf)
-START "ServerSync Server" java -jar %serversync% server
+START "ServerSync" java -jar %serversync% server
 ENDLOCAL
